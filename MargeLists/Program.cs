f93d67e697142b2e9b32db85d65e925021f414d6
MaggieA23 <m.georgieva2019@pmg-pernik.com>
2023-02-14 13:29:28 +0200

    add task3

---
+++ MargeLists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MargeLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = new List<int>();
+            List<string> result = Console.ReadLine().Split('|',StringSplitOptions.RemoveEmptyEntries).ToList();
+            result.Reverse();
+            for (int i = 0; i < result.Count; i++)
+            {
+                int[] temp = result[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    input.Add(temp[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
